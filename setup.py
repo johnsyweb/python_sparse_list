d25d90584dd39362112c5c3f0f754e6aa7d9192d
Pete Johns <paj-github@johnsy.com>
2013-07-14 14:03:17 +1000

    Having the version number in two places is dumb!

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from distutils.core import setup
+
+version = '0.3'
+github_url = 'https://github.com/johnsyweb/python_sparse_list'
+
 setup(
     name='sparse_list',
     py_modules=['sparse_list'],
-    version='0.2',
+    version=version,
     description='A list where most (>95%) values will be None (or default)',
     author='Pete Johns',
     author_email='paj+pypi@johnsy.com',
-    url='https://github.com/johnsyweb/python_sparse_list',
-    download_url='https://github.com/johnsyweb/python_sparse_list/tarball/0.1',
+    url=github_url,
+    download_url='{}/tarball/{}'.format(github_url, version),
     keywords=['sparse', 'list', 'container', 'iterable'],
     classifiers=[],
 )
