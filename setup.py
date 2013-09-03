7ac79c130af25eca94a1a84b2d98ca466605fc2b
Pete Johns <paj-github@johnsy.com>
2013-09-04 06:12:38 +1000

    Provide more information to PyPI about the package.

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@ from distutils.core import setup
 
 version = '0.3'
 github_url = 'https://github.com/johnsyweb/python_sparse_list'
+paj='Pete Johns'
+paj_email='paj+pypi@johnsy.com'
 
 setup(
     name='sparse_list',
     py_modules=['sparse_list'],
     version=version,
     description='A list where most (>95%) values will be None (or default)',
-    author='Pete Johns',
-    author_email='paj+pypi@johnsy.com',
+    author=paj,
+    author_email=paj_email,
+    maintainer=paj,
+    maintainer_email=paj_email,
     url=github_url,
     download_url='{}/tarball/{}'.format(github_url, version),
     keywords=['sparse', 'list', 'container', 'iterable'],
@@ -26,4 +30,8 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Utilities",
     ],
+    long_description=('{}\n'.format('-' * 80).join(
+        [open(f).read() for f in ('README.rst', 'LICENSE.txt')]
+    )),
+    license='MIT'
 )
