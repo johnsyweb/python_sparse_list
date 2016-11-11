8b529acea1706469a42f3094d82a7652c8d69f70
Pete Johns <pete.johns@envato.com>
2016-11-11 23:20:02 +1100

    Use benchmark
    Thanks @Goutte!

--- time_sparse_list.py
+++ time_sparse_list.py
@@ -1,18 +1,41 @@
 #!/usr/bin/env python
 
+import benchmark
 import sparse_list
-import timeit
 
-sl = sparse_list.SparseList(5)
-t = timeit.Timer(setup='''
-import sparse_list
-sl = sparse_list.SparseList(5)
-''',
-                 stmt='''
-repr(sl)
-''')
 
-try:
-    print t.repeat()
-except:
-    print t.print_exc()
+class Benchmark_Repr(benchmark.Benchmark):
+    def setUp(self):
+        self.sparse_list = sparse_list.SparseList(1000)
+        self.list = [None] * 1000
+
+    def test_sparse_list(self):
+        repr(self.sparse_list)
+
+    def test_list(self):
+        repr(self.list)
+
+class Benchmark_Insert(benchmark.Benchmark):
+    def setUp(self):
+        self.sparse_list = sparse_list.SparseList(1000)
+        self.list = [None] * 1000
+
+    def test_sparse_list(self):
+        self.sparse_list[100] = 'apple'
+
+    def test_list(self):
+        self.list[100] = 'apple'
+
+class Benchmark_Retrieval(benchmark.Benchmark):
+    def setUp(self):
+        self.sparse_list = sparse_list.SparseList(1000)
+        self.list = [None] * 1000
+
+    def test_sparse_list(self):
+        self.sparse_list[100]
+
+    def test_list(self):
+        self.list[100]
+
+if __name__ == '__main__':
+    benchmark.main(format="markdown", numberFormat="%.4g")
